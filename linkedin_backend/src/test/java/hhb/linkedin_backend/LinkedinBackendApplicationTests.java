c118f8192b0328530a055d7248a2fb05b78bf349
Huang Zilong <absorbgeorge@foxmail.com>
2025-02-24 09:50:46 +0800

    setup proj

---
+++ linkedin_backend/src/test/java/hhb/linkedin_backend/LinkedinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hhb.linkedin_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

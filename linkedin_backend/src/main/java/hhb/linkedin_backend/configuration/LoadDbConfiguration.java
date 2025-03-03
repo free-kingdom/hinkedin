59b80d8bb3e6bb0d0baa0593198404fe26539af1
Huang Zilong <absorbgeorge@foxmail.com>
2025-03-04 05:50:47 +0800

    pwd encoder

--- linkedin_backend/src/main/java/hhb/linkedin_backend/configuration/LoadDbConfiguration.java
+++ linkedin_backend/src/main/java/hhb/linkedin_backend/configuration/LoadDbConfiguration.java
@@ -2,17 +2,22 @@ package hhb.linkedin_backend.configuration;
 
 import hhb.linkedin_backend.features.authentication.model.AuthenticationUser;
 import hhb.linkedin_backend.features.authentication.repository.AuthenticationUserRepository;
+import hhb.linkedin_backend.features.authentication.utils.Encoder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class LoadDbConfiguration {
+    private final Encoder encoder;
 
     @Bean
     public CommandLineRunner initDatabase(AuthenticationUserRepository userRepository) {
         return args -> {
-            AuthenticationUser authenticationUser = new AuthenticationUser("hhb@rof.org", "password");
+            AuthenticationUser authenticationUser =
+                    new AuthenticationUser("hhb@rof.org", encoder.encode("password"));
             userRepository.save(authenticationUser);
         };
     }

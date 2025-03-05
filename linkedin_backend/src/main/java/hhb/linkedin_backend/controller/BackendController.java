38b5dce7bf55f248320ea990a26fe29ee30e65e7
Huang Zilong <absorbgeorge@foxmail.com>
2025-03-05 14:35:19 +0800

    add exception handlers

--- linkedin_backend/src/main/java/hhb/linkedin_backend/controller/BackendController.java
+++ linkedin_backend/src/main/java/hhb/linkedin_backend/controller/BackendController.java
@@ -2,6 +2,8 @@ package hhb.linkedin_backend.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,6 +12,25 @@ import java.util.Map;
 // 控制器顾问，捕获并格式化输出所有控制器中抛出的错误
 @ControllerAdvice
 public class BackendController {
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseEntity.badRequest().body(Map.of("message", "Required request body is missing."));
+    }
+
+    // 处理验证错误
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        StringBuilder errorMessage = new StringBuilder();
+        e.getBindingResult().getFieldErrors().forEach(error ->
+                errorMessage.append(error.getField()).append(": ").append(error.getDefaultMessage()).append("; ")
+        );
+        return ResponseEntity.badRequest().body(Map.of("message", errorMessage.toString()));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(Exception e){
+        return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
+    }
 
     // 捕获“未定义”异常
     @ExceptionHandler(Exception.class)

f210455ccd4ee578f4b5a43338ed38829b1f8508
Huang Zilong <absorbgeorge@foxmail.com>
2025-03-06 16:32:19 +0800

    begin frontend

--- linkedin_frontend/src/main.tsx
+++ linkedin_frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className="m-4 p-4 border font-bold text-xl">
+      hello react
+    </div>
   </StrictMode>,
 )
